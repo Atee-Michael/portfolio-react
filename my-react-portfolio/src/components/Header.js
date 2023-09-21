36f1106fbc293f7bce2dc24ac5a1b2235464a80a
Michael Anthony Atee <104591824+Atee-Michael@users.noreply.github.com>
2023-09-21 13:36:48 +0100

    repositioned navbar components

--- my-react-portfolio/src/components/Header.js
+++ my-react-portfolio/src/components/Header.js
@@ -4,22 +4,22 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+
 
 function Header({ setSelectedComponent }) {
   return (
     <div>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home"><button onClick={() => setSelectedComponent("Home")}>Michael Atee</button></Navbar.Brand>
+          <Navbar.Brand href=""><button onClick={() => setSelectedComponent("About")}>Michael Atee</button></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+            <Nav className="me-auto mx-auto">
               <button onClick={() => setSelectedComponent("Home")}>Home</button>
               <button onClick={() => setSelectedComponent("About")}>About Me</button>
               <button onClick={() => setSelectedComponent("Projects")}>My Projects</button>
-              <button onClick={() => setSelectedComponent("Contact")}>Contact</button>
             </Nav>
+            <button className="ml-auto" onClick={() => setSelectedComponent("Contact")}>Contact</button>
           </Navbar.Collapse>
         </Container>
       </Navbar>
